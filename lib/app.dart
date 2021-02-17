d5516ce9d4cc5aceddd5c8252af797230c6d0b17
Don-Stevenson <dsteven1@gmail.com>
2021-02-16 21:03:19 -0500

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+import 'screens/location_detail/location_detail.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LocationDetail(),
+    );
+  }
+}
